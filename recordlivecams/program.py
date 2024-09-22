82c217535ce69f5b0f4dd8102c032be3917d8dac
Flashy78 <90150289+Flashy78@users.noreply.github.com>
2024-09-22 02:00:11 -0700

    Fixing completed_path

--- recordlivecams/program.py
+++ recordlivecams/program.py
@@ -4,11 +4,6 @@ from pathlib import Path
 from recordlivecams.monitor import Monitor
 from recordlivecams.database import migrator
 
-config_path = Path("/app/config")
-config_template_path = Path("/app/recordlivecams/config_template.yaml")
-video_path = Path("/app/download")  # no trailing slash
-completed_path = Path("/app/completed")  # no trailing slash
-
 
 def main():
     # setup logger
@@ -20,10 +15,16 @@ def main():
     logger = logging.getLogger()
     logger.info("Starting up")
 
+    config_path = Path("/app/config")
+    config_template_path = Path("/app/recordlivecams/config_template.yaml")
+    video_path = Path("/app/download")  # no trailing slash
+    completed_path = Path("/app/completed")  # no trailing slash
+
     migrator.migrate(logger, config_path / "db.sqlite3")
 
     # completed_path may not be mounted into Docker, in that case just use video_path as the default location
     if not completed_path.exists():
+        logger.warning(f"completed_path not set, assigning as {video_path}")
         completed_path = video_path
 
     monitor = Monitor(
